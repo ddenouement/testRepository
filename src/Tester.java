ff2c1b00da893ac8abcb23dee9e3732cac8eeb51
Kate-Dt <31987466+Kate-Dt@users.noreply.github.com>
2018-02-12 23:42:12 +0200

    changed doProcess()

--- src/Tester.java
+++ src/Tester.java
@@ -1,17 +1,17 @@
 
 public class Tester {
+	
    public static int doProcess(int a,int b) {
 	  int r=0;
-	  a*=10;
 	  b*=0.2;
 	   
-	   return r+a+b;
+	   return r+(8*b*190);
    }
 	
 	public static void main(String[] args) {
 		int l =9;
 		int k = 122;
-	int res=	doProcess(l,k);
+	int res = doProcess(l,k);
 		System.out.println(res);
 	}
 }
